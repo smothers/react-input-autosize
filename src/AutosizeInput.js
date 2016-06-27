f47daecce6903f0c65e97951fc4abcdf6f7bcadf
Chris Smothers <chris.smothers@gmail.com>
2016-06-26 20:47:42 -0400

    string refs to callbacks

--- src/AutosizeInput.js
+++ src/AutosizeInput.js
@@ -14,7 +14,7 @@ const AutosizeInput = React.createClass({
 		]),
 		onChange: React.PropTypes.func,                  // onChange handler: function(newValue) {}
 		placeholder: React.PropTypes.string,             // placeholder text
-		placeholderIsMinWidth: React.PropTypes.string,   // don't collapse size to less than the placeholder
+		placeholderIsMinWidth: React.PropTypes.bool,   // don't collapse size to less than the placeholder
 		style: React.PropTypes.object,                   // css styles for the outer element
 		value: React.PropTypes.any,                      // field value
 	},
@@ -39,18 +39,18 @@ const AutosizeInput = React.createClass({
 		if (!this.isMounted() || !window.getComputedStyle) {
 			return;
 		}
-		const inputStyle = window.getComputedStyle(this.refs.input);
+		const inputStyle = window.getComputedStyle(this._input);
 		if (!inputStyle) {
 			return;
 		}
-		const widthNode = this.refs.sizer;
+		const widthNode = this._sizer;
 		widthNode.style.fontSize = inputStyle.fontSize;
 		widthNode.style.fontFamily = inputStyle.fontFamily;
 		widthNode.style.fontWeight = inputStyle.fontWeight;
 		widthNode.style.fontStyle = inputStyle.fontStyle;
 		widthNode.style.letterSpacing = inputStyle.letterSpacing;
 		if (this.props.placeholder) {
-			const placeholderNode = this.refs.placeholderSizer;
+			const placeholderNode = this._placeholderSizer;
 			placeholderNode.style.fontSize = inputStyle.fontSize;
 			placeholderNode.style.fontFamily = inputStyle.fontFamily;
 			placeholderNode.style.fontWeight = inputStyle.fontWeight;
@@ -59,14 +59,14 @@ const AutosizeInput = React.createClass({
 		}
 	},
 	updateInputWidth () {
-		if (!this.isMounted() || typeof this.refs.sizer.scrollWidth === 'undefined') {
+		if (!this.isMounted() || typeof this._sizer.scrollWidth === 'undefined') {
 			return;
 		}
 		let newInputWidth;
 		if (this.props.placeholder && (!this.props.value || (this.props.value && this.props.placeholderIsMinWidth))) {
-			newInputWidth = Math.max(this.refs.sizer.scrollWidth, this.refs.placeholderSizer.scrollWidth) + 2;
+			newInputWidth = Math.max(this._sizer.scrollWidth, this._placeholderSizer.scrollWidth) + 2;
 		} else {
-			newInputWidth = this.refs.sizer.scrollWidth + 2;
+			newInputWidth = this._sizer.scrollWidth + 2;
 		}
 		if (newInputWidth < this.props.minWidth) {
 			newInputWidth = this.props.minWidth;
@@ -78,16 +78,16 @@ const AutosizeInput = React.createClass({
 		}
 	},
 	getInput () {
-		return this.refs.input;
+		return this._input;
 	},
 	focus () {
-		this.refs.input.focus();
+		this._input.focus();
 	},
 	blur () {
-		this.refs.input.blur();
+		this._input.blur();
 	},
 	select () {
-		this.refs.input.select();
+		this._input.select();
 	},
 	render () {
 		const sizerValue = (this.props.defaultValue || this.props.value || '');
@@ -96,11 +96,11 @@ const AutosizeInput = React.createClass({
 		const inputStyle = Object.assign({}, this.props.inputStyle);
 		inputStyle.width = this.state.inputWidth + 'px';
 		inputStyle.boxSizing = 'content-box';
-		const placeholder = this.props.placeholder ? <div ref="placeholderSizer" style={sizerStyle}>{this.props.placeholder}</div> : null;
+		const placeholder = this.props.placeholder ? <div ref={(c) => { this._placeholderSizer = c }} style={sizerStyle}>{this.props.placeholder}</div> : null;
 		return (
 			<div className={this.props.className} style={wrapperStyle}>
-				<input {...this.props} ref="input" className={this.props.inputClassName} style={inputStyle} />
-				<div ref="sizer" style={sizerStyle}>{sizerValue}</div>
+				<input {...this.props} ref={(c) => { this._input = c }} className={this.props.inputClassName} style={inputStyle} />
+				<div ref={(c) => { this._sizer = c }} style={sizerStyle}>{sizerValue}</div>
 				{placeholder}
 			</div>
 		);
